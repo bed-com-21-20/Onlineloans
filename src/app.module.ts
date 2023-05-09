55278113f1b3caaf6600c7907e8fd6e9099493a1
Felix Liwonde <bed-com-21-20@unima.ac.mw>
2023-05-09 13:37:30 +0200

    Database connected successfully

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
 import { UsersModule } from './==dry=run/users/users.module';
-//import { CustomersModule } from './customers/customers.module';
+import { CustomersService } from './customers/customers.service';
+import { TypeOrmModule} from '@nestjs/typeorm';
+import entities from './customers/typeorm';
 
 @Module({
-  imports: [UsersModule],
+ /// imports: [UsersModule],
+  imports: [ TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'onlineloan',
+    entities,
+    synchronize: true,
+
+  }),
+],
+
   controllers: [AppController],
   providers: [AppService],
 })
--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -7,18 +7,7 @@ import entities from './typeorm';
 
 
 @Module({
-  imports: [CustomersService , CustomersModule,TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '',
-    database: 'onlineloan',
-    entities,
-    synchronize: true,
-
-  }),
-],
+ 
   controllers: [CustomersController],
   providers: [],
 })
