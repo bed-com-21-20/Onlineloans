1803ef1e51cacbfbee9ec0e81784871b2f19c4b4
bed-com-21-20 <127156083+bed-com-21-20@users.noreply.github.com>
2023-06-09 23:30:23 +0200

    Update auth.module.ts

--- src/auth/auth/auth.module.ts
+++ src/auth/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from '../local.strategy';
+import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersService } from 'src/User/users.service';
