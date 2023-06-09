32f36cc514d1d535dc27c25968c9dcb94eb4cc74
bed-com-21-20 <127156083+bed-com-21-20@users.noreply.github.com>
2023-06-09 23:20:05 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,6 @@ import { LendersController } from './lenders/lenders/lenders.controller';
 import { customers } from './User/typeorm';
 import { lenders } from './User/typeorm/lenders';
 import { UsersController } from './User/users.controller';
-import { AuthController } from './Auth/auth/auth.controller';
 
 @Module({
  
